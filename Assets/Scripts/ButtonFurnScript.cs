c2e335a475db494788fa100565fa7bb51878c8b2
NatashaKSS <xnagol@gmail.com>
2018-02-26 19:07:41 +0800

    Added basic place furniture on any plane feature

---
+++ Assets/Scripts/ButtonFurnScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFurnScript : MonoBehaviour {
+
+    public FurnitureManager FurnitureManager;
+    public GameObject furn;
+
+    public void SelectFurn() {
+        FurnitureManager.SetCurrSelectedFurn(furn);
+    }
+}
---
+++ Assets/Scripts/FurnitureManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Responsible for managing what furniture items are rendered into the scene 
+ * and how they are rendered by controlling the various furniture prefabs and 
+ * furniture selecter GUI.
+ */
+public class FurnitureManager : MonoBehaviour {
+
+    public GameObject HitCubeParent;
+    public GameObject FurniturePlaceholder;
+    public GameObject DefaultFurniture;
+    public GameObject CurrSelectedFurniture;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    //=================================================
+    // GETTERS & SETTERS
+    //=================================================
+
+    public GameObject GetCurrSelectedFurn() {
+        return CurrSelectedFurniture;
+    }
+
+    public void SetCurrSelectedFurn(GameObject furn) {
+        CurrSelectedFurniture = furn;
+    }
+
+    //=================================================
+    // GENERATE FURNITURE
+    //=================================================
+
+    public void GenerateFurniture() {
+        GameObject newFurniture = Instantiate(CurrSelectedFurniture);
+        newFurniture.transform.SetPositionAndRotation(HitCubeParent.transform.position, Quaternion.AngleAxis(-90.0f, Vector3.right));
+        // newFurniture.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f); // HitCubeParent's scale
+    }
+
+}
