0cdc3c29b3774449fadec50408f4085a0e36f622
NatashaKSS <xnagol@gmail.com>
2018-02-27 02:05:32 +0800

    Added Delete button

---
+++ Assets/Scripts/DeleteButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteButtonScript : MonoBehaviour {
+
+    private GameObject camera;
+    private GameObject thisFurniture;
+
+	void Start () {
+        thisFurniture = transform.parent.transform.parent.gameObject;
+        camera = GameObject.Find("Main Camera");
+	}
+
+    private void Update() {
+        transform.LookAt(camera.transform);
+        transform.rotation = Quaternion.LookRotation(transform.position - camera.transform.position);
+    }
+
+    public void DeleteThisFurniture() {
+        Destroy(thisFurniture, 0.5f);
+    }
+
+}
